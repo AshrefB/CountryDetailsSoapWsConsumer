26fa399a16a0f009f0817d42a9d99b12aa5577ea
Achref Bouadila <a_bouadila@live.fr>
2020-02-23 21:16:50 +0100

    first commit

---
+++ src/main/java/com/ashref/soap/ws/CountryDetailsSoapWsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.ashref.soap.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryDetailsSoapWsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryDetailsSoapWsConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashref/soap/ws/CountryDetailsSoapWsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashref.soap.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryDetailsSoapWsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
